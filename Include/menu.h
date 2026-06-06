f9d074f574d1b51e4a66d7f2d4d10bcdc531791a
watchmypizza <kevinosswald@proton.me>
2026-06-06 17:09:35 +0200

    Initialized empty branch documentation

--- Include/menu.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <yaml-cpp/yaml.h>
-#include "typedefs.h"
-
-int menu(void);
-YAML::Node loadYaml(const str_t& path);
-void RAMYAML(void);
--- Include/typedefs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <variant>
-#include <vector>
-
-typedef std::string str_t;
-typedef std::vector<str_t> strlist_t;
-
-using stringvec = std::variant<strlist_t, str_t>;
